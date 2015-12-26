ef19a91dacc7a858a3d262dfadf482f46e02016e
Martin Ostertag <martin.ostertag@gmx.de>
2015-12-26 15:18:06 +0100

    changed DLL export macro to better name

--- cedarJpegLib.h
+++ cedarJpegLib.h
@@ -5,7 +5,7 @@
 #include <EGL/eglext.h>
 #include <stdint.h>
 
-#define EXPORT __attribute__ ((visibility ("default")))
+#define DLL_EXPORT __attribute__ ((visibility ("default")))
 
 typedef void *CEDAR_JPEG_HANDLE;
 
@@ -13,17 +13,17 @@ typedef void *CEDAR_JPEG_HANDLE;
 extern "C"
 {
 #endif
-EXPORT CEDAR_JPEG_HANDLE cedarInitJpeg(EGLDisplay eglD);
-EXPORT int cedarLoadJpeg(CEDAR_JPEG_HANDLE handle, const char *filename);
-EXPORT int cedarLoadMem(CEDAR_JPEG_HANDLE handle, uint8_t *buf, size_t size);
-EXPORT int cedarDecodeJpeg(CEDAR_JPEG_HANDLE handle, int width, int height);
-EXPORT int cedarDecodeJpegToMem(CEDAR_JPEG_HANDLE handle, int width, int height, char *mem);
-EXPORT int cedarCloseJpeg (CEDAR_JPEG_HANDLE handle);
-EXPORT int cedarDestroyJpeg(CEDAR_JPEG_HANDLE handle);
-EXPORT void cedarGetEglImage(CEDAR_JPEG_HANDLE, void **egl_image);
-EXPORT int cedarGetOrientation(CEDAR_JPEG_HANDLE);
-EXPORT int cedarGetWidth(CEDAR_JPEG_HANDLE);
-EXPORT int cedarGetHeight(CEDAR_JPEG_HANDLE);
+DLL_EXPORT CEDAR_JPEG_HANDLE cedarInitJpeg(EGLDisplay eglD);
+DLL_EXPORT int cedarLoadJpeg(CEDAR_JPEG_HANDLE handle, const char *filename);
+DLL_EXPORT int cedarLoadMem(CEDAR_JPEG_HANDLE handle, uint8_t *buf, size_t size);
+DLL_EXPORT int cedarDecodeJpeg(CEDAR_JPEG_HANDLE handle, int width, int height);
+DLL_EXPORT int cedarDecodeJpegToMem(CEDAR_JPEG_HANDLE handle, int width, int height, char *mem);
+DLL_EXPORT int cedarCloseJpeg (CEDAR_JPEG_HANDLE handle);
+DLL_EXPORT int cedarDestroyJpeg(CEDAR_JPEG_HANDLE handle);
+DLL_EXPORT void cedarGetEglImage(CEDAR_JPEG_HANDLE, void **egl_image);
+DLL_EXPORT int cedarGetOrientation(CEDAR_JPEG_HANDLE);
+DLL_EXPORT int cedarGetWidth(CEDAR_JPEG_HANDLE);
+DLL_EXPORT int cedarGetHeight(CEDAR_JPEG_HANDLE);
 #if __cplusplus
 }
 #endif
